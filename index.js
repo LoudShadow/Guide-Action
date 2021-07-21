f868dcf00d57a7751ed6eec8edca3d5a5df90896
Joseph Evans <josephmevans2002@gmail.com>
2021-07-21 01:09:10 +0100

    test 4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const path = require("path");
 // const fs = require('fs');
 // const path = require("path");
 
@@ -22,10 +24,14 @@ try {
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
     
 
+    fs.readdir(process.env.GITHUB_WORKSPACE, (err, files) => {
+        files.forEach(file => {
+          console.log(file);
+        });
+    });
+    console.log(`The event payload: ${payload}`);
   } 
 
 catch (error) {
