ce5f155244e99ff35e4dafd7499220cb0b986c80
Joseph Evans <josephmevans2002@gmail.com>
2021-07-21 13:48:14 +0100

    test20

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ try {
     //console.log('Environment vars:'+process.env.GITHUB_WORKSPACE);
 
     readDirs('process.env.GITHUB_WORKSPACE',[path.resolve(process.env.GITHUB_WORKSPACE,'node_modules')],function(err,files){
+      console.log(files);
       for(const file of files){
         if (file.endsWith(".md")){
           console.log(file);
